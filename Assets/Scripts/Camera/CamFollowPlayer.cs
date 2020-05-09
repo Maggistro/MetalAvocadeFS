f3f2927e32951e35a1db512fb5b71059e8c67832
ufcyg <mellowski@gmail.com>
2020-05-09 10:51:21 +0200

    cam follow player

---
+++ Assets/Scripts/Camera/CamFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Avocado
+{
+    public class CamFollowPlayer : MonoBehaviour
+    {
+        public float offset;
+        [SerializeField] private Transform player;
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        private void Update()
+        {
+            transform.position = new Vector3(player.position.x - offset, player.position.y + offset, player.position.z - offset);
+        }
+    }
+}
