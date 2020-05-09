3ff02099391780bcc6c90bfa36bb73f3f06ae96a
ufcyg <mellowski@gmail.com>
2020-05-09 11:03:50 +0200

    player moving

--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     Vector2 movementVector = Vector2.zero;
+    Rigidbody rigidbody;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         if (Input.GetAxis("Horizontal") != 0)
@@ -16,5 +21,6 @@ public class CharacterController : MonoBehaviour
             movementVector = new Vector2(movementVector.x, Input.GetAxis("Vertical"));
         }
         Debug.Log(movementVector);
+        rigidbody.AddForce(movementVector);
     }
 }
