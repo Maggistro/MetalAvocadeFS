eccb7f62e276bd53b55785e60cf85c5fa9069cc8
Maggistro <maggistro@googlemail.com>
2020-05-10 09:26:05 +0200

    Add avocado

---
+++ Assets/Scripts/Camera/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Avocado
+{
+    public class Billboard : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+            this.GetComponent<SpriteRenderer>().flipX = this.GetComponentInParent<NpcController>().transform.forward.x < 0;
+        }
+    }
+}
