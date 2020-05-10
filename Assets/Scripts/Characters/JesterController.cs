ad226cf70a182aab53cc91702c911f13abb8bad9
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-05-10 12:38:52 +0200

    Jester restored, merged JesterController changes from npcs branch

--- Assets/Scripts/Characters/JesterController.cs
+++ Assets/Scripts/Characters/JesterController.cs
@@ -7,11 +7,33 @@ namespace Avocado
     public class JesterController : NpcController
     {
         private GameObject avocado;
+        private Renderer[] renderers;
+        private Renderer spriteNormal;
+        private Renderer spritePainting;
+        public enum JesterState
+        {
+            NORMAL, VANDALIZING
+        }
+
 
         private new void Awake()
         {
             base.Awake();
             npcName = "Narr";
+            renderers = GetComponentsInChildren<Renderer>();
+            FindSprites();
+            SetSpriteStates(JesterState.NORMAL);
+        }
+
+        private void FindSprites()
+        {
+            Transform sNormal = transform.Find("SpriteNormal");
+            Transform sPainting = transform.Find("SpritePainting");
+            if (sNormal != null && sPainting != null)
+            {
+                spriteNormal = sNormal.GetComponent<Renderer>();
+                spritePainting = sPainting.GetComponent<Renderer>();
+            }
         }
 
         new void Update()
@@ -35,6 +57,34 @@ namespace Avocado
         public void Vandalize()
         {
             Debug.Log("Vandalizing stuff");
+            SetSpriteStates(JesterState.VANDALIZING);
+        }
+
+        private void SetSpriteStates(JesterState newJesterState)
+        {
+            if (spriteNormal != null && spritePainting != null)
+            {
+                switch (newJesterState)
+                {
+                    case JesterState.NORMAL:
+                        spriteNormal.enabled = true;
+                        spritePainting.enabled = false;
+                        break;
+                    case JesterState.VANDALIZING:
+                        spriteNormal.enabled = false;
+                        spritePainting.enabled = true;
+                        break;
+                }
+            }
+        }
+
+        public void SetVisibility(bool visible)
+        {
+            Debug.Log(string.Format("Setting visibility of jester to {0}", visible.ToString()));
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = visible;
+            }
         }
     }
 }
