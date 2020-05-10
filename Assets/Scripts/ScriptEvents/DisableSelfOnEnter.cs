d0cfd61fdc18f2dfe0cedcefc6888e369d8c93e3
Maggistro <maggistro@googlemail.com>
2020-05-10 11:02:14 +0200

    Added cheap hide outside mask

---
+++ Assets/Scripts/ScriptEvents/DisableSelfOnEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Avocado
+{
+    public class DisableSelfOnEnter : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.GetComponentInParent<CharacterController>()) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
