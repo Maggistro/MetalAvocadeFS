bafac3877239d9648bb24afd9ccac18041c2d060
Maggistro <maggistro@googlemail.com>
2020-05-10 09:55:09 +0200

    Implement avocado pickup

--- Assets/Scripts/Characters/JesterController.cs
+++ Assets/Scripts/Characters/JesterController.cs
@@ -6,15 +6,30 @@ namespace Avocado
 {
     public class JesterController : NpcController
     {
+        private GameObject avocado;
+
         private new void Awake()
         {
             base.Awake();
             npcName = "Narr";
         }
 
+        new void Update()
+        {
+            base.Update();
+            if (avocado != null) {
+                MoveAvocado();
+            }
+        }
+
         public void PickupAvocado()
         {
-            Debug.Log("picking up avocado");
+            avocado = GameObject.FindGameObjectsWithTag("Avocado")[0];
+        }
+
+        void MoveAvocado()
+        {
+            avocado.transform.position = Vector3.Lerp(avocado.transform.position, transform.position + (Vector3.up * .75f), .5f);
         }
 
         public void Vandalize()
