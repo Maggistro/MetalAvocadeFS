d7f90a0afa2ab1c634e6d4cbda6ecffc8e8176ee
ufcyg <mellowski@gmail.com>
2020-05-10 17:35:14 +0200

    boatfix/waterfix

---
+++ Assets/Scripts/Core/WaterCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollider : MonoBehaviour
+{
+    [SerializeField] BoxCollider left;
+    [SerializeField] BoxCollider right;
+    [SerializeField] BoxCollider front;
+    [SerializeField] BoxCollider back;
+    [SerializeField] BoxCollider up;
+
+    private void AdjustColliders()
+    {
+        Bounds bounds = GetComponent<Mesh>().bounds;
+        Vector3 extents = bounds.extents;//     The extents of the Bounding Box. This is always half of the size of the Bounds.
+        //up set center z
+    }
+}
