10af648eaeaeff273d49ba4d2431055ee17db3f6
ufcyg <mellowski@gmail.com>
2020-05-10 19:36:26 +0200

    fixes volume adjust

---
+++ Assets/Scripts/Core/Adjustvolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adjustvolume : MonoBehaviour
+{
+    float masterVolume = .5f;
+    void Start()
+    {
+        AudioSource[] sources = GameObject.FindObjectsOfType<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            source.volume = masterVolume;
+        }
+    }
+}
